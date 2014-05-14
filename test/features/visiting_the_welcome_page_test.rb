177ffda6e6cab9c278a08e0841678b39c265ce4a
CADBOT <charliec364@gmail.com>
2014-05-14 09:17:41 +0000

    Try to break shippable by including a broken test

--- test/features/visiting_the_welcome_page_test.rb
+++ test/features/visiting_the_welcome_page_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 feature "Visiting The Welcome Page" do
   scenario "The welcome page has my name on it" do
-    visit root_path
+    #visit root_path
+    raise 'Break to see if it breaks shipabble tests'
     page.must_have_content "CADBOT"
   end
 end
