8d7c6bb979dd0e45458edf0c7bfdf77a8ac19ffb
CADBOT <charliec364@gmail.com>
2014-05-14 09:19:15 +0000

    Break shippable again without using a raise

--- test/features/visiting_the_welcome_page_test.rb
+++ test/features/visiting_the_welcome_page_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 feature "Visiting The Welcome Page" do
   scenario "The welcome page has my name on it" do
-    #visit root_path
-    raise 'Break to see if it breaks shipabble tests'
+    visit root_path
     page.must_have_content "CADBOT"
+    page.must_have_content "This should break it"
   end
 end
