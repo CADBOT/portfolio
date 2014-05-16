2f848b4c52a3d7dba01a840718c44effee59a6a2
CADBOT <charliec364@gmail.com>
2014-05-16 04:06:45 +0000

    first test set pass for feature

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,27 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def new
+    @project = Project.new
+  end
+  
+  def create
+    @project = Project.new(params[:object])
+    if @project.save
+      flash[:notice] = 'Project has been created.'
+      redirect_to @project
+    else
+    end
+  end
+  
+  def show
+    @project = Project.find(params[:id])    
+  end
+
+  private
+    
+    def project_params
+      params.require(:project).permit(:name, :technologies_used)
+    end
+
 end
--- test/features/projects/creating_a_project_test.rb
+++ test/features/projects/creating_a_project_test.rb
@@ -1,12 +1,14 @@
 require "test_helper"
-
-feature "Projects::CreatingAProject" do
-  scenario "I want to create a new project" do
+            
+feature "As the site owner, I want to add a portfolio item so that I can show off my work" do
+  scenario "adding a new project" do
     visit projects_path
-    click_on 'New Project'
-    fill_in 'Name', with: 'Code Fellows Portfolio'
-    fill_in 'Technologies used', with: 'Rails, ruby, foundation'
-    click_on 'Create Project'
-    page.text.must_include 'Project has been created'
+    click_on "New project"
+    fill_in "Name", with: "Code Fellows Portfolio"
+    fill_in "Technologies used", with: "Rails, Ruby, Bootstrap, HTML5, CSS3"
+    click_on "Create Project"
+    page.text.must_include "Project has been created"
+    #assert page.has_css?("#notice"), "Expected a flash notice on this page, none found."
+    page.status_code.must_equal 200
   end
 end
